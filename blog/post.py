b695f6e226a5dfc23e0ff2ac765314c51f975996
Erin Novoa <68754679+eknovoa@users.noreply.github.com>
2022-06-10 15:40:56 -0500

    updates

--- blog/post.py
+++ blog/post.py
@@ -4,9 +4,11 @@ class Post:
         self.title = title
         self.content = content
 
-
     def json(self):
         return {'title': self.title,
                 'content': self.content,
-        }
+                }
+
+
+
 
