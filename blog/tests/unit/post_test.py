a3dca6a910877723aec2f45b15f7f29db2e03c15
Erin Novoa <68754679+eknovoa@users.noreply.github.com>
2022-06-10 15:04:05 -0500

    Update post_test.py

--- blog/tests/unit/post_test.py
+++ blog/tests/unit/post_test.py
@@ -7,4 +7,8 @@ class PostTest(TestCase):
         p = Post('Test', 'Test Content')
         self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
-        pass
+
+        def test_json(self):
+        p = Post('Test', 'Test Content')
+        expected = {'title': 'Test', 'content': 'Test Content'}
+        self.assertDictEqual(expected, p.json())
